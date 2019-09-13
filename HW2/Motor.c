05ba7d040443ccbe6ad61cabf66496d84727c9ee
Austen Green <A.G-ZOE@outlook.com>
2019-09-13 13:00:11 -0500

    Homework 3

---
+++ HW2/Motor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+double motor(double A, double c, double t)
+{
+	return A * (1 - exp(c * t));
+}
+
+
+int main()
+{
+	FILE* f = fopen("motor.csv", "wt");
+	double Ts = 0.01;
+	if (!f)
+	{
+		printf("Unable to open file.\n");
+		return 1;
+	}
+	for (double time = 0; time < 5; time += Ts)
+	{
+		double out = 0;
+		out = motor(7.2, -0.7, time);
+		fprintf(f, "%.2f,%.2f,%.2f\n", time, 7.2, out);
+	}
+	fclose(f);
+	return 0;
+}
